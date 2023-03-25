aa0cbbfafa0716c3f112cc3806fd6d503f67329b
MADElinessss <jyseen@naver.com>
2023-03-25 14:30:29 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 신정연 on 2023/03/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
